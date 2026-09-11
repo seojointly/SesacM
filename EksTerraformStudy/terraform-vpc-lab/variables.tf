66e3d461856eea6cc79150dfe200c3ee44c58c68
seojointly <seojointly@gmail.com>
2026-09-11 11:26:36 +0900

    20260910 Terraform Study

---
+++ EksTerraformStudy/terraform-vpc-lab/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS 리전 정보(서울 리전)"
+  type        = string
+  default     = "ap-northeast-2"
+}
+
+variable "vpc_cidr" {
+  description = "VPC_CIDR 블록"
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public Subnet CIDR 목록 (가용 영역 a, c 순)"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "private Subnet CIDR 목록 (가용 영역 a, c 순)"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "project_name" {
+  description = "프로젝트 이름(프리픽스)"
+  type = string
+  default = "my-eks"
+}
